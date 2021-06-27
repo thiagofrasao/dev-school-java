1acacb9b5cf979487eff8e0c5cc09ec3c705d348
Thiago Frasão <85502294+thiagofrasao@users.noreply.github.com>
2021-06-27 16:53:13 -0300

    Coomit new project CRUD

---
+++ maven-jdbc/src/main/java/digytal/model/Produto.java
@@ -0,0 +1,46 @@
+/*package digytal.model;
+
+public class Cadastro {
+	
+	private Integer id;
+	private String artista;
+	private String album;
+	private String genero;
+	private Integer ano;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getArtista() {
+		return artista;
+	}
+	public void setArtista(String artista) {
+		this.artista = artista;
+	}
+	public String getAlbum() {
+		return album;
+	}
+	public void setAlbum(String album) {
+		this.album = album;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	public Integer getAno() {
+		return ano;
+	}
+	public void setAno(Integer ano) {
+		this.ano = ano;
+	}
+	
+	
+	
+
+}
+*/
